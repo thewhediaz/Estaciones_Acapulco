983fcfd939f31cf03751bf0e54b81cd081cd2bf7
thewhediaz <123022766+thewhediaz@users.noreply.github.com>
2025-09-29 12:30:04 -0600

    st.py

--- st.py
+++ st.py
@@ -11,7 +11,7 @@ alerta = True
 if alerta:
     st.markdown(
         """
-        <div style="width:100%;height:150px;background:red;color:white;font-size:40px;text-align:center;animation: blink 0.5s infinite;">
+        <div style="width:100%;height:150px;background:red;color:white;font-size:40px;text-align:center;animation: blink 0.1s infinite;">
             ¡ALERTA! 
         </div>
         <style>
@@ -210,6 +210,7 @@ fig.update_yaxes(title=f"{variable_col}", dtick=dtick_y, gridcolor="rgba(229,236
 col_vacia_izq, col_central, col_vacia_der = st.columns([1, 10, 1])
 with col_central:
     st.plotly_chart(fig, use_container_width=False, config={"responsive": False, "displayModeBar": True})
+
 
 
 
