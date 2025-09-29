215259a1e47ac81138ae82e619c8fe349ef96f2f
thewhediaz <123022766+thewhediaz@users.noreply.github.com>
2025-09-29 12:41:44 -0600

    st.py

--- st.py
+++ st.py
@@ -9,16 +9,15 @@ import pytz
 alerta = True
 
 if alerta:
-    texto = "¡ALERTA! Todos los usuarios deben prestar atención a los datos de monitoreo"
-    # Calculamos un tiempo aproximado en segundos según la cantidad de caracteres
-    tiempo_animacion = max(10, len(texto) * 0.5)  # 0.5s por caracter, mínimo 10s
+    texto = "¡ALERTA MÁXIMA! Todos los usuarios deben prestar atención a los datos de monitoreo"
+    tiempo_animacion = max(10, len(texto) * 0.5)
 
     st.markdown(
         f"""
         <div style="width:100%;height:150px;background:red;overflow:hidden;position:relative;animation: blink 1s infinite;">
-            <p style="position:absolute;white-space:nowrap;animation: scrollText {tiempo_animacion}s linear infinite;font-size:40px;color:white;">
-                {texto}
-            </p>
+            <div style="display:inline-block; white-space:nowrap; animation: scrollText {tiempo_animacion}s linear infinite; font-size:40px; color:white;">
+                {texto}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <!-- Espacio extra para separación -->
+            </div>
         </div>
 
         <style>
@@ -35,6 +34,7 @@ if alerta:
         """,
         unsafe_allow_html=True
     )
+
 
 else:
     # Crear cabecera con logos
@@ -223,6 +223,7 @@ fig.update_yaxes(title=f"{variable_col}", dtick=dtick_y, gridcolor="rgba(229,236
 col_vacia_izq, col_central, col_vacia_der = st.columns([1, 10, 1])
 with col_central:
     st.plotly_chart(fig, use_container_width=False, config={"responsive": False, "displayModeBar": True})
+
 
 
 
